9930c2ec5816563c8073f4e2b1c4412397d1f6a5
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-05-26 14:36:38 +0530

    category card

--- Frontend/Eshop/src/pages/Home.jsx
+++ Frontend/Eshop/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Carousal from '../components/Carousal'
+import CatogoryCard from '../components/CategoryCard'
 
 
 const Home = () => {
   return (
     <>
     <Carousal />
+    <CatogoryCard />
     </>
   )
 }
