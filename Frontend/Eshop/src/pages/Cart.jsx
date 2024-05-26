4edc4cfb52f55e14357525cab099b6b441207bf8
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-05-26 17:45:44 +0530

    cart ui

--- Frontend/Eshop/src/pages/Cart.jsx
+++ Frontend/Eshop/src/pages/Cart.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CartList from '../components/CartList'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div>
+      <CartList/>
+    </div>
   )
 }
 
