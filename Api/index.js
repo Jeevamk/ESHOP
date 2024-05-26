8e8914fc8ccc1d76033da714debc9d8629a289ad
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-05-26 17:29:51 +0530

    connect

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,13 @@ const express = require ('express')
 const app = express()
 const PORT = process.env.PORT || 8001;
 const morgan = require('morgan')
+const dotenv = require('dotenv').config({path:'config.env'})
+const path = require('path')
+
+
+app.get('/',(req,res)=>{
+    res.status(200).json({'message':'success'})
+})
 
 
 
